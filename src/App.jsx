342072121ce3fd65f023b4b917dcc953bfe69f64
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-10-29 03:40:20 +0800

    Add LoadingScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import './App.css'
+import LoadingScreen from './components/LoadingScreen';
+import './index.css';
 
 function App() {
-    return <></>;
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    return (
+        <>
+            {/* {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>} */}
+            {!isLoaded ? (
+                <LoadingScreen onComplete={() => setIsLoaded(true)} />
+            ) : (
+                <div className="text-center text-white mt-20 inter-medium">
+                    <h1>IS THIS INTER????</h1>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default App
