b3074ca993693ea4b2aa01c91f01560b47f97a27
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-10-30 02:00:18 +0800

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,23 @@ import HomeScreen from './components/sections/Home';
 import AboutScreen from './components/sections/About';
 import ContactScreen from './components/sections/Contact';
 import ProjectScreen from './components/sections/Projects';
+import Navbar from './components/Navbar';
 
 function App() {
     const [isLoaded, setIsLoaded] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <>
-            {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
-            {/* {!isLoaded ? (
-                <LoadingScreen onComplete={() => setIsLoaded(true)} />
-            ) : (
-                <div className="text-center text-white mt-20 inter-medium">
-                    <h1>IS THIS INTER????</h1>
-                </div>
-            )} */}
-            <HomeScreen/>
-            <AboutScreen/>
-            <ContactScreen/>
-            <ProjectScreen/>
+            {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+            <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? 'opapcity-100' : 'opacity-0'} bg-black text-gray-100`}>
+                <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+                <HomeScreen />
+                <AboutScreen />
+                <ContactScreen />
+                <ProjectScreen />
+
+            </div>
         </>
     );
 }
