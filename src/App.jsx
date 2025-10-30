8e24e444c635fd2f047a76254644436b6e0c3b9a
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-10-31 03:51:23 +0800

    Add buttons to Home

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ function App() {
             <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}>
                 <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
                 <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-                <div className='h-screen' >this is a divvvvv</div>
                 <Home/>
                 <About />
                 <Contact />
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,8 +1,23 @@
 const Home = () => {
     return (
-        <>
-            <div>This is HOME screen</div>
-        </>
+        <section id="home" className="min-h-screen flex items-center justify-center relative">
+            <div className="text-center z-10 px-4">
+                <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-linear-to-r from-orange-700 to-orange-500 bg-clip-text text-transparent leading-right">Hi, I'm FNC shiro.</h1>
+                <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto font-light">
+                    I'm a full-stack developer who loves crafting clean, scalable web
+                    applications. My goal is to build solutions that offer both
+                    exceptional performance and a delightful user experience.
+                </p>
+                <div className="flex justify-center space-x-4">
+                    <a href="#projects" className="bg-orange-500 text-white py-3 px-5 rounded font-medium transition relative overflow-hidden 
+                                                hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(255, 105, 0, 0.4)]"
+                    >View Projects</a>
+                    <a href="#contact" className="border border-orange-500/50 text-orange-50 py-3 px-6 rounded font-medium transition-all duration-200 
+                                                hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(255, 105, 0, 0.2)] hover:bg-orange-500/10"
+                    >View Projects</a>
+                </div>
+            </div>
+        </section>
     )
 }
 export default Home
