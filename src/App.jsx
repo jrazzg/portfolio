f71f5c91dc339a198ab5e5a00b7ab3e23b33fe2f
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-10-31 03:33:52 +0800

    Implement mobile navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AboutScreen from './components/sections/About';
 import ContactScreen from './components/sections/Contact';
 import ProjectScreen from './components/sections/Projects';
 import Navbar from './components/Navbar';
+import MobileMenu from './components/MobileMenu';
 
 function App() {
     const [isLoaded, setIsLoaded] = useState(false);
@@ -17,6 +18,7 @@ function App() {
             {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
             <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}>
                 <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+                <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
                 <div className='h-screen' >this is a divvvvv</div>
                 <HomeScreen/>
                 <AboutScreen />
