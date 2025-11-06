8f0aba6460638147af3e845ce91b1b7cd9ed4ec9
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-07 03:52:27 +0800

    Update Home.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -2,22 +2,16 @@ import RevealOnScroll from '../RevealOnScroll';
 
 const Home = () => {
     return (
-        <section id="home" className="min-h-screen flex items-center justify-center relative">
-            <RevealOnScroll>
-                <div className="text-center z-10 px-4">
-                    <h2 className="text-orange-600 text-lg uppercase tracking-widest">Nigga Boy</h2>
-                    <h1 className="text-8xl">Joshua Razzi Garingo</h1>
-                    {/* <div className="flex justify-center space-x-4">
-                        <a href="#projects" className="bg-orange-500 text-white py-3 px-5 rounded font-medium transition relative overflow-hidden 
-                                                hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(255, 105, 0, 0.4)]"
-                        >View Projects</a>
-                        <a href="#contact" className="border border-orange-500/50 text-orange-50 py-3 px-6 rounded font-medium transition-all duration-200 
-                                                hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(255, 105, 0, 0.2)] hover:bg-orange-500/10"
-                        >View Projects</a>
-                    </div> */}
-                </div>
-            </RevealOnScroll>
+        <section id="home" className="min-h-screen flex flex-col">
+            <div className="flex flex-1 justify-center items-center">
+                <RevealOnScroll>
+                    <h1 className="text-7xl text-orange-600 text-center">Hey, I'm Richard,<br />a software developer.</h1>
+                </RevealOnScroll>
+            </div>
+            <div className='flex justify-center items-center'>
+                    <h2 className="uppercase tracking-widest mb-8">fueled by curiosity, driven by code.</h2>
+            </div>
         </section>
     )
 }
-export default Home
+export default Home;
