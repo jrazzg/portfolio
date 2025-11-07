6c89aa41a4c499ed2ec94ffd79b650c42ccb7a7d
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-08 01:55:08 +0800

    Modify project cards

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -2,64 +2,42 @@ import RevealOnScroll from "../RevealOnScroll"
 
 const Project = () => {
     return (
-        <>
-            <section
-                id="projects"
-            >
-                <RevealOnScroll className="min-h-screen flex items-center justify-ceneter py-20">
-                    <div className="max-w-5xl mx-auto px-4">
-                        <h2 className="text-3xl font-bold mb-8 bg-linear-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">Featured Projects</h2>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,224,0.2)] transition-all">
-                                <h3 className="text-xl font-bold mb-2">JPG Attendance</h3>
-                                <p className="text-gray-400 mb-4">An offline-first Android mobile app designed for monitoring university club's members.</p>
-                                <div className="flex flex-wrap gap-2 mb-4">
-                                    {['React', 'Node.js', 'AWS'].map((item, key) => (
-                                        <span
-                                            key={key}
-                                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
-                                                        hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,224,0.2)] transition">{item}</span>
-                                    ))}
-                                </div>
-                                <div className="flex justify-between items-center">
-                                    <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project</a>
-                                </div>
+            <section id="projects" className="min-h-screen py-20">
+                <RevealOnScroll>
+                    <div className="max-w-5xl mx-auto px-4 mt-5">
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                            {/* JPG ATTENDANCE */}
+                            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-orange-500/30 transition-all">
+                                <span className="bg-white h-6xl w-6xl"></span>
+                                <h3 className="text-xl font-bold mb-1">JPG Attendance</h3>
+                                <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
+                                                hover:bg-orange-500/20 transition uppercase"
+                                >
+                                    mobile
+                                </span>
                             </div>
-                            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,224,0.2)] transition-all">
-                                <h3 className="text-xl font-bold mb-2">JPG Attendance</h3>
-                                <p className="text-gray-400 mb-4">An offline-first Android mobile app designed for monitoring university club's members.</p>
-                                <div className="flex flex-wrap gap-2 mb-4">
-                                    {['React', 'Node.js', 'AWS'].map((item, key) => (
-                                        <span
-                                            key={key}
-                                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
-                                                        hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,224,0.2)] transition">{item}</span>
-                                    ))}
-                                </div>
-                                <div className="flex justify-between items-center">
-                                    <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project</a>
-                                </div>
+                            {/* NDDU ATTENDANCE */}
+                            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-orange-500/30 transition-all">
+                                <h3 className="text-xl font-bold mb-1">NDDU Attendance</h3>
+                                <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
+                                                hover:bg-orange-500/20 transition uppercase"
+                                >
+                                    web
+                                </span>
                             </div>
-                            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,224,0.2)] transition-all">
-                                <h3 className="text-xl font-bold mb-2">JPG Attendance</h3>
-                                <p className="text-gray-400 mb-4">An offline-first Android mobile app designed for monitoring university club's members.</p>
-                                <div className="flex flex-wrap gap-2 mb-4">
-                                    {['React', 'Node.js', 'AWS'].map((item, key) => (
-                                        <span
-                                            key={key}
-                                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
-                                                        hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,224,0.2)] transition">{item}</span>
-                                    ))}
-                                </div>
-                                <div className="flex justify-between items-center">
-                                    <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project</a>
-                                </div>
+                            {/* BLAAN APP */}
+                            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-orange-500/30 transition-all">
+                                <h3 className="text-xl font-bold mb-1">Blaan App</h3>
+                                <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
+                                                hover:bg-orange-500/20 transition uppercase"
+                                >
+                                    mobile
+                                </span>
                             </div>
                         </div>
                     </div>
                 </RevealOnScroll>
             </section>
-        </>
     )
 }
 export default Project
