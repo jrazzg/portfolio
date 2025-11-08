4d4cec73aca6c141f23e9a8e412d350092c21353
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-09 02:44:35 +0800

    Add video

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,5 +1,8 @@
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
+import isoUlt from "../../assets/videos/iso_ult.mp4"
+import neonUlt from "../../assets/videos/neon_ult.mp4"
+import sovaUlt from "../../assets/videos/sova_ult.mp4"
 
 const Projects = () => {
     const targetRef = useRef(null);
@@ -19,31 +22,61 @@ const Projects = () => {
             >
                 <div className="flex flex-1 max-w-6xl">
                     {/* JPG ATTENDANCE */}
-                    <a href="#" className="flex-1 m-6 p-6 rounded-xl border bg-black border-white/10 hover:-translate-y-5 hover:border-orange-500/30 transition-all">
-                        <h3 className="text-xl font-bold mb-1">JPG Attendance</h3>
-                        <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
+                    <a href="#" className="relative flex-1 items-end m-6 rounded-xl border border-white/10 hover:-translate-y-5 hover:border-orange-500/30 transition-all overflow-hidden">
+                        <video
+                            src={isoUlt}
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                            className="absolute inset-0 w-full h-full object-cover z-0"
+                        />
+                        <div className="text-center absolute bottom-0 right-0 left-0 z-10 p-6 bg-linear-to-t from-black/90 to-transparent">
+                            <h3 className="text-xl font-bold mb-1">JPG Attendance</h3>
+                            <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
                                                 hover:bg-orange-500/20 transition uppercase"
-                        >
-                            mobile
-                        </span>
+                            >
+                                mobile
+                            </span>
+                        </div>
                     </a>
                     {/* NDDU ATTENDANCE */}
-                    <a href="#" className="flex-1 m-6 p-6 rounded-xl border bg-black border-white/10 hover:-translate-y-5 hover:border-orange-500/30 transition-all">
-                        <h3 className="text-xl font-bold mb-1">NDDU Attendance</h3>
-                        <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
+                    <a href="#" className="relative flex-1 items-end m-6 rounded-xl border border-white/10 hover:-translate-y-5 hover:border-orange-500/30 transition-all overflow-hidden">
+                        <video
+                            src={neonUlt}
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                            className="absolute inset-0 w-full h-full object-cover z-0"
+                        />
+                        <div className="text-center absolute bottom-0 right-0 left-0 z-10 p-6 bg-linear-to-t from-black/90 to-transparent">
+                            <h3 className="text-xl font-bold mb-1">NDDU Attendance</h3>
+                            <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
                                                 hover:bg-orange-500/20 transition uppercase"
-                        >
-                            web
-                        </span>
+                            >
+                                web
+                            </span>
+                        </div>
                     </a>
                     {/* BLAAN APP */}
-                    <a href="#" className="flex-1 m-6 p-6 rounded-xl border bg-black border-white/10 hover:-translate-y-5 hover:border-orange-500/30 transition-all">
-                        <h3 className="text-xl font-bold mb-1">Blaan App</h3>
-                        <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
+                    <a href="#" className="relative flex-1 items-end m-6 rounded-xl border border-white/10 hover:-translate-y-5 hover:border-orange-500/30 transition-all overflow-hidden">
+                        <video
+                            src={sovaUlt}
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                            className="absolute inset-0 w-full h-full object-cover z-0"
+                        />
+                        <div className="text-center absolute bottom-0 right-0 left-0 z-10 p-6 bg-linear-to-t from-black/90 to-transparent">
+                            <h3 className="text-xl font-bold mb-1">Blaan App</h3>
+                            <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
                                                 hover:bg-orange-500/20 transition uppercase"
-                        >
-                            mobile
-                        </span>
+                            >
+                                mobile
+                            </span>
+                        </div>
                     </a>
                 </div>
             </motion.div>
