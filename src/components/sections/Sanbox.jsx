66ca1caee6b14ffaf23bba9d5ae7db4b2b382756
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-08 18:16:24 +0800

    Adjust hover transform

--- src/components/sections/Sanbox.jsx
+++ src/components/sections/Sanbox.jsx
@@ -14,11 +14,11 @@ const Sandbox = () => {
         <section ref={targetRef} className="relative h-[50vh] overflow-visible">
             <motion.div
                 style={{ y }}
-                className="h-full flex justify-center pointer-events-none"
+                className="h-full flex justify-center"
             >
                 <div className="flex flex-1 max-w-6xl">
                     {/* JPG ATTENDANCE */}
-                    <div className="flex-1 m-6 p-6 rounded-xl border bg-black border-white/10 hover:-translate-y-1 hover:border-orange-500/30 transition-all">
+                    <div className="flex-1 m-6 p-6 rounded-xl border bg-black border-white/10 hover:-translate-y-5 hover:border-orange-500/30 transition-all">
                         <h3 className="text-xl font-bold mb-1">JPG Attendance</h3>
                         <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
                                                 hover:bg-orange-500/20 transition uppercase"
@@ -27,7 +27,7 @@ const Sandbox = () => {
                         </span>
                     </div>
                     {/* NDDU ATTENDANCE */}
-                    <div className="flex-1 m-6 p-6 rounded-xl border bg-black border-white/10 hover:-translate-y-1 hover:border-orange-500/30 transition-all">
+                    <div className="flex-1 m-6 p-6 rounded-xl border bg-black border-white/10 hover:-translate-y-5 hover:border-orange-500/30 transition-all">
                         <h3 className="text-xl font-bold mb-1">NDDU Attendance</h3>
                         <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
                                                 hover:bg-orange-500/20 transition uppercase"
@@ -36,7 +36,7 @@ const Sandbox = () => {
                         </span>
                     </div>
                     {/* BLAAN APP */}
-                    <div className="flex-1 m-6 p-6 rounded-xl border bg-black border-white/10 hover:-translate-y-1 hover:border-orange-500/30 transition-all">
+                    <div className="flex-1 m-6 p-6 rounded-xl border bg-black border-white/10 hover:-translate-y-5 hover:border-orange-500/30 transition-all">
                         <h3 className="text-xl font-bold mb-1">Blaan App</h3>
                         <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
                                                 hover:bg-orange-500/20 transition uppercase"
