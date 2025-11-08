641a95fef9a810dfd289db1c74437baed8e09486
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-09 03:24:38 +0800

    Update About.jsx

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,71 +1,16 @@
+import RevealOnScroll from '../RevealOnScroll';
+
 const About = () => {
 
     const frontendSkills = ['React', 'TailewindCSS'];
     const backendSkills = ['Python', 'C++', 'Firebase'];
 
     return (
-        <section
-            id="about"
-            className="flex items-center justify-center"
-        >
-            <div className="max-w-3xl mx-auto px-4">
-                <h2 className="text-3xl font-bold mb-8 bg-linear-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">About Me</h2>
-                <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-                    <p className="text-gray-300 mb-6">
-                        Passionate developer with expertise in building scalable
-                        web applicaitons and creating innovative solutions.
-                    </p>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                            <h3 className="text-xl font-bold mb-4">Frontend</h3>
-                            <div className="flex flex-wrap gap-2">
-                                {
-                                    frontendSkills.map((item, key) => (
-                                        <span 
-                                        key={key}
-                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
-                                                        hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,224,0.2)] transition">{item}</span>
-                                    ))
-                                }
-                            </div>
-                        </div>
-                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                            <h3 className="text-xl font-bold mb-4">Backend</h3>
-                            <div className="flex flex-wrap gap-2">
-                                {
-                                    backendSkills.map((item, key) => (
-                                        <span 
-                                        key={key}
-                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
-                                                        hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,224,0.2)] transition">{item}</span>
-                                    ))
-                                }
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-all">
-                            <h3 className="text-xl font-bold mb-4">Education</h3>
-                            <ul className="list-disc list-inside text-gray-300 space-y-2">
-                                <li>
-                                    <strong>BS Computer Science</strong> - Notre Dame of Dadiangas University 
-                                    (2021-2025)
-                                </li>
-                                <li>Relevant Coursework: Datastructures, Web Developpment, Cloud Computing.</li>
-                            </ul>
-                        </div>
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-all">
-                            <h3 className="text-xl font-bold mb-4">Work Experience</h3>
-                            <div className="space-y-4 text-gray-300">
-                                <div>
-                                    <h4 className="font-bold">IT Intern at Dole Philippines, Inc. (July 2024 - August 2024)</h4>
-                                    <p>Develop and maintained internal systems including AutoCap and HR-Pro2e.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <section id="home" className="min-h-[50vh] w-[60%] flex flex-col mx-auto">
+            <h2 className="uppercase tracking-widest mb-6">Beyond the projects &mdash;</h2>
+            <RevealOnScroll><h1 className="text-7xl text-orange-600">Developer by choice.</h1></RevealOnScroll>
+            <RevealOnScroll><h1 className="text-7xl text-orange-600">Creator by nature.</h1></RevealOnScroll>
+            <RevealOnScroll><h1 className="text-7xl text-orange-600">Always learning. Always building.</h1></RevealOnScroll>
         </section>
     )
 }
