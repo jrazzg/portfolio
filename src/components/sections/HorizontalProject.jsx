98a149ebd88a60a4983778fbe887e0ce8bb5c140
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-08 17:47:42 +0800

    Add project card animation

--- src/components/sections/HorizontalProject.jsx
+++ src/components/sections/HorizontalProject.jsx
@@ -4,7 +4,7 @@ import { useRef } from "react";
 const HorizontalProject = () => {
     const targetRef = useRef(null);
     const { scrollYProgress } = useScroll({ target: targetRef });
-    const x = useTransform(scrollYProgress, [0, 1], ["3%", "-65%"]);
+    const x = useTransform(scrollYProgress, [0, 1], ["3%", "-75%"]);
 
     return (
         <section ref={targetRef} className="relative h-[300vh] bg-orange-600">
--- src/components/sections/Sanbox.jsx
+++ src/components/sections/Sanbox.jsx
@@ -8,47 +8,41 @@ const Sandbox = () => {
         offset: ["start start", "end end"]
     });
 
-    const y = useTransform(scrollYProgress, [0, 1], ["0%", "-50%"]);
+    const y = useTransform(scrollYProgress, [0, 1], ["0%", "-40%"]);
 
     return (
-        <section ref={targetRef} className="relative h-[200vh]">
+        <section ref={targetRef} className="relative h-[50vh]">
             <motion.div
                 style={{ y }}
-                className="sticky top-0 h-screen flex items-center justify-center text-5xl text-white"
+                className="h-full flex justify-center"
             >
-                <div className="flex flex-col">
-                    <p>Smooth Scroll Animation</p>
-                    <div className="max-w-5xl mx-auto px-4 mt-5">
-                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                            {/* JPG ATTENDANCE */}
-                            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-orange-500/30 transition-all">
-                                <span className="bg-white h-6xl w-6xl"></span>
-                                <h3 className="text-xl font-bold mb-1">JPG Attendance</h3>
-                                <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
+                <div className="flex flex-1 max-w-6xl">
+                    {/* JPG ATTENDANCE */}
+                    <div className="flex-1 m-6 p-6 rounded-xl border bg-black border-white/10 hover:-translate-y-1 hover:border-orange-500/30 transition-all">
+                        <h3 className="text-xl font-bold mb-1">JPG Attendance</h3>
+                        <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
                                                 hover:bg-orange-500/20 transition uppercase"
-                                >
-                                    mobile
-                                </span>
-                            </div>
-                            {/* NDDU ATTENDANCE */}
-                            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-orange-500/30 transition-all">
-                                <h3 className="text-xl font-bold mb-1">NDDU Attendance</h3>
-                                <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
+                        >
+                            mobile
+                        </span>
+                    </div>
+                    {/* NDDU ATTENDANCE */}
+                    <div className="flex-1 m-6 p-6 rounded-xl border bg-black border-white/10 hover:-translate-y-1 hover:border-orange-500/30 transition-all">
+                        <h3 className="text-xl font-bold mb-1">NDDU Attendance</h3>
+                        <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
                                                 hover:bg-orange-500/20 transition uppercase"
-                                >
-                                    web
-                                </span>
-                            </div>
-                            {/* BLAAN APP */}
-                            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-orange-500/30 transition-all">
-                                <h3 className="text-xl font-bold mb-1">Blaan App</h3>
-                                <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
+                        >
+                            web
+                        </span>
+                    </div>
+                    {/* BLAAN APP */}
+                    <div className="flex-1 m-6 p-6 rounded-xl border bg-black border-white/10 hover:-translate-y-1 hover:border-orange-500/30 transition-all">
+                        <h3 className="text-xl font-bold mb-1">Blaan App</h3>
+                        <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
                                                 hover:bg-orange-500/20 transition uppercase"
-                                >
-                                    mobile
-                                </span>
-                            </div>
-                        </div>
+                        >
+                            mobile
+                        </span>
                     </div>
                 </div>
             </motion.div>
