1ba3a4feb955e1ca538e0ea0d0250cdb069ea89b
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-10 22:35:08 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,25 +7,23 @@ const Navbar = ({ menuOpen, setMenuOpen }) => {
     }, [menuOpen]);
 
     return (
-        <div className="flex justify-center">
-            <nav className="fixed top-5 w-[60%] px-4 rounded-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
-                <div className="mx-auto px-4">
-                    <div className="flex justify-between items-center h-16">
-                        <a href="#home">
-                            <Logo className="w-10 h-10 fill-orange-600"/>
-                        </a>
+        <nav className="fixed w-full py-5 px-8 z-40 backdrop-blur-sm">
+            <div className="flex justify-between items-center">
+                <a href="#home">
+                    <Logo className="w-10 h-10 fill-white" />
+                </a>
 
-                        <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
-                            &#9776;
-                        </div>
-                        <div className="hidden md:flex items-center space-x-8">
-                            <a href="#contact" className="text-gray-300 hover:text-white transition-colors">VIEW RESUME</a>
-                            <a href="#contact" className="text-gray-300 hover:text-white transition-colors">LET'S TALK</a>
-                        </div>
-                    </div>
+                <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
+                    &#9776;
                 </div>
-            </nav>
-        </div>
+                <div className="hidden md:flex items-center space-x-3">
+                    <a href="#contact" className="text-white hover:bg-orange-600 transition-all 
+                                                            px-5 py-2 rounded-full backdrop-blur-lg">VIEW RESUME</a>
+                    <a href="#contact" className="text-black hover:text-white hover:bg-orange-600 transition-all
+                                                            px-5 py-2 rounded-full bg-white backdrop-blur-lg">LET'S TALK</a>
+                </div>
+            </div>
+        </nav>
     );
 };
 export default Navbar;
