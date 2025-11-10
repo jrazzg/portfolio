da34f07787dab51f751ea65bade6a3a89bf456e8
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-10 22:39:38 +0800

    Remove loading effect

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,8 @@ function App() {
 
     return (
         <>
-            {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
             <ReactLenis root />
-            <div className={`min-h-screen transition-opacity duration-700
-                            ${isLoaded ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100 relative`}>
+            <div className={"min-h-screen transition-opacity duration-700 bg-black text-gray-100 relative"}>
                 <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
                 <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
                 <Home />
