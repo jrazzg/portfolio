ed0280dbcfbd061ebc8e1951a04bc14a983261b6
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-11 02:59:45 +0800

    Add icons to footer

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,13 +1,38 @@
 import RevealOnScroll from "../RevealOnScroll"
+import emailIcon from "../../assets/icons/email-new.svg"
+import githubIcon from "../../assets/icons/github.svg"
+import linkedinIcon from "../../assets/icons/linkedin.svg"
 
 const Contact = () => {
     return (
         <div className="min-h-[40vh] w-[60%] flex flex-col py-30 mx-auto">
             <RevealOnScroll><h1 className="text-7xl text-black mb-6">Let's build something together.</h1></RevealOnScroll>
             <div className="flex tracking-widest text-white">
-                <a href="#" className="mr-10">sigma@gmail.com</a>
-                <a href="#" className="mr-10">/LINKEDIN</a>
-                <a href="#" className="mr-10">/GITHUB</a>
+                <a
+                    href="mailto:joshuarazzi.garingo@gmail.com"
+                    className="mr-8 flex items-center gap-2 text-lg -tracking-normal"
+                >
+                    <img src={emailIcon} alt="email-icon" className="inline-block h-[2em] w-[2em]" />
+                    joshuarazzi.garingo@gmail.com
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/jrgaringo/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-2 mr-8 text-lg -tracking-normal"
+                >
+                    <img src={linkedinIcon} alt="linkedin-icon" className="inline-block h-[2em] w-[2em]" />
+                    LinkedIn
+                </a>
+                <a
+                    href="https://github.com/jrazzg"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-2 text-lg -tracking-normal"
+                >
+                    <img src={githubIcon} alt="email-icon" className="inline-block h-[2em] w-[2em] fill-white" />
+                    GitHub
+                </a>
             </div>
         </div>
     )
