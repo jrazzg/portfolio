f9024fd0fcc6fa3d7c422f124b5b029fd8d026c2
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-13 00:20:03 +0800

    Add design to words

--- src/components/sections/ProjectsTransition.jsx
+++ src/components/sections/ProjectsTransition.jsx
@@ -10,8 +10,9 @@ const PorjectsTransition = () => {
         <section ref={targetRef} className="relative h-[300vh]" style={{ backgroundImage: 'linear-gradient(to bottom, #f54900, #ff6b1a, #b93400, #000000)', }}>
             <div className="sticky top-0 flex h-screen items-center overflow-hidden">
                 <motion.div style={{ x }} className="flex">
-                    <h1 className="text-[18rem] tracking-tight whitespace-nowrap bg-linear-to-r from-white/80 via-[#ffb17c] to-[#ffc4aab2] bg-clip-text text-transparent">
-                        Crafting ideas into interactive experiences.
+                    <h1 className="text-[18rem] tracking-tight whitespace-nowrap bg-linear-to-r from-white/80 via-[#ffb17c] to-[#ffffff] bg-clip-text text-transparent">
+                        Crafting <span className="inline-block text-[16rem] bg-[#fbff00] text-[#ff7424] rotate-5 font-light mr-10">ideas</span>
+                        into <span className="inline-block bg-linear-to-r font-bold from-white/10 to-white decoration-[#f36115]/20 underline bg-clip-text text-transparent">interactive experiences.</span>
                     </h1>
                 </motion.div>
             </div>
