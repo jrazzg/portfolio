fe94458283844ede060ca50b93df2011e7fc6470
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-13 02:49:42 +0800

    Rephrase

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -9,8 +9,10 @@ const Home = () => {
             >
                 <div className='flex flex-col px-[20%]'>
                     {/* <RevealOnScroll><h1 className="mr-1 uppercase tracking-widest text-white">Hey. I'm <span className='text-orange-500 font-bold'>Razzi</span></h1></RevealOnScroll> */}
-                    <RevealOnScroll><h1 className="text-4xl pb-3 tracking-tight bg-linear-to-r font-bold from-orange-500 to-orange-600/60 bg-clip-text text-transparent">Hey, I'm Razzi.</h1></RevealOnScroll>
-                    <RevealOnScroll><h1 className="text-7xl tracking-tight leading-[.9] bg-linear-to-r font-bold from-gray-200 to-white/60 bg-clip-text text-transparent">I turn ideas into software, and errors into lessons.</h1></RevealOnScroll>
+                    <RevealOnScroll><h1 className="text-4xl pb-3 tracking-tight bg-linear-to-r font-bold from-orange-500 to-orange-600/60 bg-clip-text text-transparent"
+                    >Hey, I'm Razzi.</h1></RevealOnScroll>
+                    <RevealOnScroll><h1 className="text-7xl tracking-tight leading-[.9] bg-linear-to-r font-bold from-gray-200 to-white/60 bg-clip-text text-transparent"
+                    >I build software that turns vision into reality.</h1></RevealOnScroll>
                 </div>
             </section>
         </>
