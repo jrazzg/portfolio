23823b07f277823c410a045ab8b30774441d40c8
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-12 21:33:35 +0800

    Change padding structure

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,12 +1,8 @@
 import RevealOnScroll from '../RevealOnScroll';
 
 const About = () => {
-
-    const frontendSkills = ['React', 'TailewindCSS'];
-    const backendSkills = ['Python', 'C++', 'Firebase'];
-
     return (
-        <div className="min-h-[65vh] w-[60%] flex flex-col mx-auto">
+        <div className="min-h-[65vh] px-[20%] flex flex-col mx-auto">
             <h2 className="uppercase tracking-widest mb-6">Beyond the projects &mdash;</h2>
             <RevealOnScroll><h1 className="text-7xl tracking-tight text-orange-600">Developer by choice.</h1></RevealOnScroll>
             <RevealOnScroll><h1 className="text-7xl tracking-tight text-orange-600">Creator by nature.</h1></RevealOnScroll>
