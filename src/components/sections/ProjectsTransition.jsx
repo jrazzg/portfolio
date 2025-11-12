02502da615830d44013681d4042150482bf55241
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-12 21:21:47 +0800

    Update ProjectsTransition.jsx

--- src/components/sections/ProjectsTransition.jsx
+++ src/components/sections/ProjectsTransition.jsx
@@ -1,21 +1,23 @@
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 
-const PorjecstTransition = () => {
+const PorjectsTransition = () => {
     const targetRef = useRef(null);
     const { scrollYProgress } = useScroll({ target: targetRef });
     const x = useTransform(scrollYProgress, [0, 1], ["3%", "-70%"]);
 
     return (
-        <section ref={targetRef} className="relative h-[300vh] bg-orange-600">
+        <section ref={targetRef} className="relative h-[300vh]" style={{ backgroundImage: 'linear-gradient(to bottom, #f54900, #ff6b1a, #b93400, #000000)', }}>
             <div className="sticky top-0 flex h-screen items-center overflow-hidden">
                 <motion.div style={{ x }} className="flex">
-                    <h1 className="text-[15rem] text-black tracking-tight whitespace-nowrap">Crafting ideas into interactive experiences.</h1>
+                    <h1 className="text-[18rem] tracking-tight whitespace-nowrap bg-linear-to-r from-white/80 via-[#ffb17c] to-[#ffc4aab2] bg-clip-text text-transparent">
+                        Crafting ideas into interactive experiences.
+                    </h1>
                 </motion.div>
             </div>
         </section>
     );
 };
 
-export default PorjecstTransition;
+export default PorjectsTransition;
 
