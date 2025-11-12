210d9236854351e8aeacdfc3d6a835db4e517821
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-12 16:27:38 +0800

    Add moving gradient background

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,13 +1,37 @@
-import RevealOnScroll from '../RevealOnScroll';
+import { motion, useMotionTemplate, useMotionValue, animate } from "framer-motion";
+import { useEffect } from "react";
+
+const COLORS = [
+    // "#ff8c42", // bright pumpkin orange
+    "#ff6b1a", // warm vibrant orange
+    "#f54900", // your main color (deep vivid orange)
+    "#b93400"  // dark burnt orange for depth
+]
 
 const Home = () => {
+    const color = useMotionValue(COLORS[0]);
+    const backgroundImage = useMotionTemplate`radial-gradient(100% 100% at 50% 0%, #000 50%, ${color})`;
+
+    useEffect(() => {
+        animate(color, COLORS, {
+            ease: 'easeInOut',
+            duration: 10,
+            repeat: Infinity,
+            repeatType: "mirror",
+        })
+    })
+
     return (
-        <section id="home" className="h-screen w-[60%] flex items-center mx-auto">
-            <div className='space-y-3'>
-                <h1 className="uppercase tracking-widest mb-6">Hey, I'm Razzi.</h1>
-                <h2 className="text-7xl text-orange-600">I turn ideas into software, and errors into lessons.</h2>
+        <motion.section
+            id="home"
+            style={{ backgroundImage, }}
+            className="h-screen flex items-center mx-auto"
+        >
+            <div className='px-[20%] space-y-3 text-center'>
+                <h1 className="uppercase tracking-widest mb-6 text-white">Hey, I'm Razzi.</h1>
+                <h2 className="text-7xl bg-linear-to-b from-orange-700 to-orange-600 bg-clip-text text-transparent">I turn ideas into software, and errors into lessons.</h2>
             </div>
-        </section>
+        </motion.section>
     )
 }
 export default Home;
