d0ac89202426de47539292537cf338c5f88e9c89
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-12 17:48:48 +0800

    Add hover effects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({ menuOpen, setMenuOpen }) => {
         <nav className="fixed w-full py-5 px-8 z-40 backdrop-blur-sm">
             <div className="flex justify-between items-center">
                 <a href="#home">
-                    <Logo className="w-10 h-10 fill-white" />
+                    <Logo className="w-10 h-10 fill-white hover:w-12 duration-500" />
                 </a>
 
                 <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
@@ -18,9 +18,9 @@ const Navbar = ({ menuOpen, setMenuOpen }) => {
                 </div>
                 <div className="hidden md:flex items-center space-x-3">
                     <a  href="#contact" 
-                        className="text-white hover:bg-orange-600 transition-all px-5 py-2 backdrop-blur-lg">VIEW RESUME</a>
+                        className="text-white  hover:bg-orange-600 hover:text-black duration-500 transition-all px-5 py-2 backdrop-blur-lg">VIEW RESUME</a>
                     <a  href="mailto:joshuarazzi.garingo@gmail.com" 
-                        className="text-black hover:text-white hover:bg-orange-600 transition-all px-5 py-2 bg-white backdrop-blur-lg">LET'S TALK</a>
+                        className="text-black hover:bg-orange-600 duration-500 transition-all px-5 py-2 bg-white backdrop-blur-lg">LET'S TALK</a>
                 </div>
             </div>
         </nav >
