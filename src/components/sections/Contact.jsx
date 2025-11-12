1840ef3b78558dab29d8a31859c14aa72b196df7
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-12 19:10:37 +0800

    Add logo to footer

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -2,6 +2,7 @@ import RevealOnScroll from "../RevealOnScroll"
 import emailIcon from "../../assets/icons/email-new.svg"
 import githubIcon from "../../assets/icons/github.svg"
 import linkedinIcon from "../../assets/icons/linkedin.svg"
+import Logo from "../../assets/logo.svg?react";
 import { motion, useMotionTemplate, useMotionValue, animate } from "framer-motion";
 import { useEffect } from "react";
 
@@ -41,37 +42,40 @@ const Contact = () => {
 
     return (
         <>
-            <motion.section style={{ backgroundImage }} className="min-h-[40vh] p-[20%] flex flex-col py-30 mx-auto">
-                <RevealOnScroll><h1 className="text-7xl text-black mb-6 tracking-tight">Let's build something together.</h1></RevealOnScroll>
-                <div className="flex tracking-widest text-black font-semibold">
-                    <a
-                        href="mailto:joshuarazzi.garingo@gmail.com"
-                        className="mr-5 flex items-center gap-2 text-lg -tracking-normal duration-500 hover:bg-white pr-2"
-                    >
-                        <img src={emailIcon} alt="email-icon" className="inline-block h-[2em] w-[2em]" />
-                        joshuarazzi.garingo@gmail.com
-                    </a>
-                    <a
-                        href="https://www.linkedin.com/in/jrgaringo/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="mr-5 flex items-center gap-2 text-lg -tracking-normal duration-500 hover:bg-white pr-2"
-                    >
-                        <img src={linkedinIcon} alt="linkedin-icon" className="inline-block h-[2em] w-[2em]" />
-                        LinkedIn
-                    </a>
-                    <a
-                        href="https://github.com/jrazzg"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="flex items-center gap-2 text-lg -tracking-normal duration-500 hover:bg-white pr-2"
-                    >
-                        <img src={githubIcon} className="inline-block h-[2em] w-[2em]" />
-                        GitHub
-                    </a>
-                </div>
-            </motion.section>
+            <div className="relative overflow-hidden">
+                <motion.section style={{ backgroundImage }} className="relative min-h-[40vh] p-[20%] flex flex-col py-30 mx-auto z-10">
+                    <RevealOnScroll><h1 className="text-7xl text-black mb-6 tracking-tight z-20">Let's build something together.</h1></RevealOnScroll>
+                    <div className="flex tracking-widest text-black font-semibold z-20">
+                        <a
+                            href="mailto:joshuarazzi.garingo@gmail.com"
+                            className="mr-5 flex items-center gap-2 text-lg -tracking-normal duration-500 hover:bg-white pr-2"
+                        >
+                            <img src={emailIcon} alt="email-icon" className="inline-block h-[2em] w-[2em]" />
+                            joshuarazzi.garingo@gmail.com
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/jrgaringo/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="mr-5 flex items-center gap-2 text-lg -tracking-normal duration-500 hover:bg-white pr-2"
+                        >
+                            <img src={linkedinIcon} alt="linkedin-icon" className="inline-block h-[2em] w-[2em]" />
+                            LinkedIn
+                        </a>
+                        <a
+                            href="https://github.com/jrazzg"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="flex items-center gap-2 text-lg -tracking-normal duration-500 hover:bg-white pr-2"
+                        >
+                            <img src={githubIcon} className="inline-block h-[2em] w-[2em]" />
+                            GitHub
+                        </a>
+                    </div>
+                    <Logo className="absolute top-0 right-0 h-[150%] fill-white/5 stroke-[2px] stroke-white/20 -z-10" />
+                </motion.section>
+            </div>
         </>
     )
 }
-export default Contact
+export default Contact;
