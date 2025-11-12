20cc86c8659dbce9d191cbfcb90e93963f67e026
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-12 21:26:18 +0800

    Add revealOnScroll

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,3 +1,5 @@
+import RevealOnScroll from '../RevealOnScroll';
+
 const Home = () => {
     return (
         <section
@@ -5,8 +7,8 @@ const Home = () => {
             className="h-screen flex items-center mx-auto"
         >
             <div className='px-[20%] space-y-3 text-center'>
-                <h1 className="uppercase tracking-widest mb-6 text-white">Hey, I'm Razzi.</h1>
-                <h2 className="text-7xl tracking-tight bg-linear-to-b from-orange-700 to-orange-600 bg-clip-text text-transparent">I turn ideas into software, and errors into lessons.</h2>
+                <RevealOnScroll><h1 className="uppercase tracking-widest mb-6 text-white">Hey, I'm Razzi.</h1></RevealOnScroll>
+                <RevealOnScroll><h2 className="text-7xl tracking-tight bg-linear-to-b from-orange-700 to-orange-600 bg-clip-text text-transparent">I turn ideas into software, and errors into lessons.</h2></RevealOnScroll>
             </div>
         </section>
     )
