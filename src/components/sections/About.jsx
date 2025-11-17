6ee041fc5f3176a959699f4a3e0bbaad60178b6a
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-17 18:27:36 +0800

    Change to black

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,13 +2,16 @@ import RevealOnScroll from '../RevealOnScroll';
 
 const About = () => {
     return (
-        <div className="h-screen px-[20%] flex flex-col mx-auto justify-center bg-white">
+        <div 
+            className="h-screen px-[20%] flex flex-col mx-auto justify-center"
+            style={{ background: "radial-gradient(120% 60% at 50% 110%, rgba(255,255,255,0.1) 5%, rgba(0,0,0,1) 100%)" }}
+        >
             {/* <h2 className="uppercase tracking-widest mb-6">Beyond the projects &mdash;</h2> */}
             <RevealOnScroll><h1 className="text-4xl pb-3 tracking-tight bg-linear-to-r from-orange-500 to-orange-600/60 bg-clip-text text-transparent"
-                    >Beyond the projects &mdash;</h1></RevealOnScroll>
-            <RevealOnScroll><h1 className="text-7xl tracking-tight leading-[.9] bg-linear-to-r from-black to-black/60 bg-clip-text text-transparent">Developer by choice.</h1></RevealOnScroll>
-            <RevealOnScroll><h1 className="text-7xl tracking-tight leading-[.9] bg-linear-to-r from-black to-black/60 bg-clip-text text-transparent">Creator by nature.</h1></RevealOnScroll>
-            <RevealOnScroll><h1 className="text-7xl tracking-tight leading-[.9] bg-linear-to-r from-black to-black/60 bg-clip-text text-transparent">Always learning. Always building.</h1></RevealOnScroll>
+            >Beyond the projects &mdash;</h1></RevealOnScroll>
+            <RevealOnScroll><h1 className="text-7xl tracking-tight leading-none bg-linear-to-r from-white to-white/60 bg-clip-text text-transparent">Developer by choice.</h1></RevealOnScroll>
+            <RevealOnScroll><h1 className="text-7xl tracking-tight leading-none bg-linear-to-r from-white to-white/60 bg-clip-text text-transparent">Creator by nature.</h1></RevealOnScroll>
+            <RevealOnScroll><h1 className="text-7xl tracking-tight leading-none bg-linear-to-r from-white to-white/60 bg-clip-text text-transparent">Always learning. Always building.</h1></RevealOnScroll>
         </div>
     )
 }
