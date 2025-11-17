10ac13894c392420816504fc62076b2057d11dbd
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-18 03:19:33 +0800

    Add dynamic width indicator

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,23 +7,34 @@ const Navbar = ({ menuOpen, setMenuOpen }) => {
     }, [menuOpen]);
 
     return (
-        <nav className="fixed w-full py-5 px-8 z-40 backdrop-blur-sm">
-            <div className="flex justify-between items-center">
-                <a href="#home">
-                    <Logo className="w-10 h-10 fill-white hover:w-12 duration-500" />
-                </a>
+        <>
+            <nav className="fixed w-full py-5 px-8 z-40 backdrop-blur-sm">
+                <div className="flex justify-between items-center">
+                    <a href="#home">
+                        <Logo className="w-10 h-10 fill-white hover:w-12 duration-500" />
+                    </a>
 
-                <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
-                    &#9776;
+                    <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
+                        &#9776;
+                    </div>
+                    <div className="hidden md:flex items-center space-x-3">
+                        <a href="#contact"
+                            className="text-white  hover:bg-orange-600 hover:text-black duration-500 transition-all px-5 py-2 backdrop-blur-lg">VIEW RESUME</a>
+                        <a href="mailto:joshuarazzi.garingo@gmail.com"
+                            className="text-black hover:bg-orange-600 duration-500 transition-all px-5 py-2 bg-white backdrop-blur-lg">LET'S TALK</a>
+                    </div>
                 </div>
-                <div className="hidden md:flex items-center space-x-3">
-                    <a  href="#contact" 
-                        className="text-white  hover:bg-orange-600 hover:text-black duration-500 transition-all px-5 py-2 backdrop-blur-lg">VIEW RESUME</a>
-                    <a  href="mailto:joshuarazzi.garingo@gmail.com" 
-                        className="text-black hover:bg-orange-600 duration-500 transition-all px-5 py-2 bg-white backdrop-blur-lg">LET'S TALK</a>
-                </div>
+            </nav >
+
+            <div className="w-full h-1 fixed top-20 z-40 
+                            bg-red-500        /* default for mobile */
+                            sm:bg-orange-500  /* small screens */
+                            md:bg-yellow-500  /* medium screens */
+                            lg:bg-green-500   /* large screens */
+                            xl:bg-blue-500    /* extra large screens */
+                            transition-colors duration-500">
             </div>
-        </nav >
+        </>
     );
 };
 export default Navbar;
