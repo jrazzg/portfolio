e0163db8c0eec535eb71b7b8319a850d16d3a6eb
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-18 03:40:28 +0800

    Add responsiveness

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -7,11 +7,9 @@ const About = () => {
             style={{ background: "radial-gradient(120% 60% at 50% 110%, rgba(255,255,255,0.1) 5%, rgba(0,0,0,1) 100%)" }}
         >
             {/* <h2 className="uppercase tracking-widest mb-6">Beyond the projects &mdash;</h2> */}
-            <RevealOnScroll><h1 className="text-4xl pb-3 tracking-tight bg-linear-to-r from-orange-500 to-orange-600/60 bg-clip-text text-transparent"
-            >Beyond the projects &mdash;</h1></RevealOnScroll>
-            <RevealOnScroll><h1 className="text-7xl tracking-tight leading-none bg-linear-to-r from-white to-white/60 bg-clip-text text-transparent">Developer by choice.</h1></RevealOnScroll>
-            <RevealOnScroll><h1 className="text-7xl tracking-tight leading-none bg-linear-to-r from-white to-white/60 bg-clip-text text-transparent">Creator by nature.</h1></RevealOnScroll>
-            <RevealOnScroll><h1 className="text-7xl tracking-tight leading-none bg-linear-to-r from-white to-white/60 bg-clip-text text-transparent">Always learning. Always building.</h1></RevealOnScroll>
+            <RevealOnScroll><p className="md:text-4xl md:pb-3 tracking-tight bg-linear-to-r from-orange-500 to-orange-600/60 bg-clip-text text-transparent"
+            >Beyond the projects &mdash;</p></RevealOnScroll>
+            <RevealOnScroll><p className="text-2xl md:text-7xl tracking-tight">Developer by choice.<br/>Creator by nature.<br/>Always learning. Always building.</p></RevealOnScroll>
         </div>
     )
 }
