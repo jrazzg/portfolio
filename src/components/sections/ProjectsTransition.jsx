a8253a6a15abc33256fd6202600649f5a718e876
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-17 18:27:22 +0800

    Change bg to black

--- src/components/sections/ProjectsTransition.jsx
+++ src/components/sections/ProjectsTransition.jsx
@@ -7,12 +7,13 @@ const PorjectsTransition = () => {
     const x = useTransform(scrollYProgress, [0, 1], ["3%", "-80%"]);
 
     return (
-        <section ref={targetRef} className="relative h-[300vh]" style={{ backgroundImage: 'linear-gradient(to bottom, #f54900, #ff6b1a, #b93400, white)', }}>
+        // <section ref={targetRef} className="relative h-[300vh]" style={{ backgroundImage: 'linear-gradient(to bottom, #f54900, #ff6b1a, #b93400, black)', }}>
+        <section ref={targetRef} className="relative h-[300vh] bg-black">
             <div className="sticky top-0 flex h-screen items-center overflow-hidden">
                 <motion.div style={{ x }} className="flex">
-                    <h1 className="text-[18rem] tracking-tight whitespace-nowrap bg-linear-to-r from-white via-[#ffb17c] to-[#f54900] bg-clip-text text-transparent">
-                        Crafting <span className="inline-block text-[16rem] bg-[#ffffff] text-[#ff7424] rotate-5 font-light mr-10">ideas</span>
-                        into <span className="inline-block bg-linear-to-r font-bold from-white/10 to-white decoration-white/20 underline bg-clip-text text-transparent">interactive experiences.</span>
+                    <h1 className="text-[18rem] tracking-tight whitespace-nowrap">
+                        Crafting <span className="inline-block text-[16rem] bg-[#f54900] mr-15">ideas</span>
+                        into <span className="inline-block decoration-[#f54900] underline">interactive experiences.</span>
                     </h1>
                 </motion.div>
             </div>
