5e3eed1c4f61222d2a057dd6fc64efbd8bffb6c8
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-19 04:24:06 +0800

    Slow cursor speed

--- src/components/CursorFollower.jsx
+++ src/components/CursorFollower.jsx
@@ -2,16 +2,17 @@ import { useEffect } from "react";
 import MouseFollower from "mouse-follower";
 import gsap from "gsap";
 import "mouse-follower/dist/mouse-follower.min.css";
+import "../index.css"
 
 MouseFollower.registerGSAP(gsap);
 
-export default function CursorFollower() {
+export default function CursorFollower() {    
     useEffect(() => {
         const cursor = new MouseFollower({
             container: document.body,
-            speed: 0.3,
+            speed: 0.9,
+            skewing: 2,
         });
-
         return () => cursor.destroy();
     }, []);
 
