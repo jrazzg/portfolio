fe74b879dec28c127d97e2dda0af31dd44db7f67
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-18 23:39:35 +0800

    Change layout

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -4,36 +4,16 @@ const Home = () => {
     return (
         <section
             id="home"
-            style={{ background: "radial-gradient(120% 60% at 50% 000%, rgba(255,255,255,0.1) 5%, rgba(0,0,0,1) 100%)" }}
-            className="h-screen flex flex-col justify-between py-5">
+            className="h-screen flex flex-col justify-between">
             <span></span>
-            <div className='flex flex-col px-[20%]'>
-                <RevealOnScroll>
-                    <h1 className="mb-3 text-7xl tracking-tight leading-[.9]">
-                        Ideas aren't enough.<br />I make them work.
-                    </h1>
-                </RevealOnScroll>
-                <RevealOnScroll>
-                    <p className="text-4xl pb-3 tracking-tight bg-linear-to-r from-orange-500 to-orange-600/60 bg-clip-text text-transparent">
-                        I'm Richard, a Software Developer.
-                    </p>
-                </RevealOnScroll>
-            </div>
-            <div className="px-8 uppercase tracking-[0.2em] text-sm font-light flex justify-between items-center">
-                <div className="hidden md:block flex-1 text-left">the journey continues below</div>
-                <div className="hidden md:block flex-1 text-center">Inspired by curiosity, driven by code</div>
-                <div className="flex-1 text-right ">
-                    <div className='inline-block'>
-                        <a href="#horizontal"
-                            className='w-8 h-8 bg-white text-black justify-center flex items-center
-                                      hover:bg-orange-600 duration-500'
-                        >↓</a>
-                    </div>
-
+            <div className='flex flex-col px-10'>
+                <div className='flex justify-between relative'>
+                    <RevealOnScroll>
+                        <h1 className="text-[18em] tracking-tighter leading-[0.8] text-[#ff5900]">Software<br/>Developer</h1>
+                    </RevealOnScroll>
+                    <RevealOnScroll><p className="absolute bottom-3 right-0 tracking-normal text-right">myemail@rzi.com<br/>linkedIn<br/>github</p></RevealOnScroll>
                 </div>
             </div>
-
-
         </section>
     );
 };
