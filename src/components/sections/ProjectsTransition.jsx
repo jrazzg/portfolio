2de56c7748f6e495c6363f9b75e4b9d8c958111d
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-19 01:20:00 +0800

    Change Color

--- src/components/sections/ProjectsTransition.jsx
+++ src/components/sections/ProjectsTransition.jsx
@@ -7,18 +7,18 @@ const ProjectsTransition = () => {
     const x = useTransform(scrollYProgress, [0, 1], ["1%", "-70%"]);
 
     return (
-        <section id="horizontal" ref={targetRef} className="relative rounded-t-[5rem] mt-5 h-[300vh] bg-[#ff5900]">
+        <section id="horizontal" ref={targetRef} className="relative mt-5 h-[300vh] bg-[#ff5900]">
             <div className="sticky top-0 flex h-screen items-center overflow-hidden text-[#0b0b0b]">
                 <motion.div style={{ x }} className="flex">
                     <h1 className="text-[18rem] tracking-tight whitespace-nowrap font-medium">
-                        Crafting <span className="inline-block text-[16rem]  bg-white mr-15">ideas</span>
+                        Crafting <span className="inline-block bg-white text-[16rem] text-[#0b0b0b] mr-15">ideas</span>
                         into <span className="inline-block decoration-white underline">interactive experiences.</span>
                     </h1>
                 </motion.div>
             </div>
         </section>
     );
-};
+};  
 
 export default ProjectsTransition;
 
