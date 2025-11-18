8568ebee4c9c77eb9cec2f617b4d7794df5a5927
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-19 02:50:02 +0800

    Change text

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,12 +2,14 @@ import RevealOnScroll from '../RevealOnScroll';
 
 const About = () => {
     return (
-        <div 
+        <div
             className="h-screen px-[20%] flex flex-col mx-auto justify-center"
         >
             <RevealOnScroll><p className="md:text-4xl md:pb-3 text-[#ff5900]"
             >Beyond the projects &mdash;</p></RevealOnScroll>
-            <RevealOnScroll><p className="text-2xl md:text-7xl tracking-tight">Developer by choice.<br/>Creator by nature.<br/>Always learning. Always building.</p></RevealOnScroll>
+            <RevealOnScroll><p className="text-2xl md:text-7xl tracking-tight">
+                A developer fueled by curiosity,<br />a creator shaped by passion.<br />Always learning. Always building.</p>
+            </RevealOnScroll>
         </div>
     )
 }
