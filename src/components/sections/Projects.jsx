4275fc20bc3410c344a7ef4ec45cd4d550282216
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-11-20 02:39:34 +0800

    Add responsiveness

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -6,7 +6,12 @@ import sovaUlt from "../../assets/videos/sova_ult.mp4"
 
 const ProjectCard = ({ name, vidSource, platform }) => {
     return (
-        <a href="#" className="relative flex-1 items-end rounded-[20px] border-10 border-white/10
+        // 340 × 656
+        <a href="#" className="relative block items-end rounded-[20px] border-10 border-white/10
+                                min-w-[238px] min-h-[455px]                        
+                                md:w-[200px] h-[382px]
+                                lg:w-[272px] lg:h-[520px]
+                                xl:w-[340px] xl:h-[650px]
                                 shadow-xl
                                 duration-500
                                 backdrop-blur-sm
@@ -20,10 +25,10 @@ const ProjectCard = ({ name, vidSource, platform }) => {
                 loop
                 muted
                 playsInline
-                className="absolute inset-0 w-full h-full object-cover z-0"
+                className="absolute w-full h-full inset-0 object-cover z-0"
             />
             <div className="text-center absolute bottom-0 right-0 left-0 z-10 p-6 bg-linear-to-t from-black/90 to-transparent">
-                <h3 className="text-2xl font-bold mb-1">{name}</h3>
+                <h3 className="text-lg md:text-2xl font-semibold mb-3 leading-[0.9]">{name}</h3>
                 <span className="bg-orange-500/10 text-orange-600 py-1 px-3 rounded-full text-sm
                                                 hover:bg-orange-500/20 transition uppercase ibm-mono"
                 >
@@ -45,21 +50,26 @@ const Projects = () => {
     const scale = useTransform(scrollYProgress, [0, 1], [0, 1]);
 
     return (
-        <section 
-            ref={targetRef} 
+        <section
+            ref={targetRef}
             className="relative overflow-visible flex items-center justify-center"
             style={{ backgroundImage: 'linear-gradient(to bottom, #ff5900, #0b0b0b 70%)', }}
-            >
+        >
             <motion.div
                 style={{ y, scale }}
-                className="hidden md:flex justify-center items-center"// md:flex 
+                className="hidden md:flex justify-center items-center"
             >
-                <div className="flex flex-1 w-290 gap-10 h-[70vh]">
+                <div className="flex flex-1 gap-[2vw]">
                     <ProjectCard name="JPG Attendance" vidSource={isoUlt} platform="mobile" />
                     <ProjectCard name="NDDU Attendance" vidSource={neonUlt} platform="web" />
                     <ProjectCard name="Blaan App" vidSource={sovaUlt} platform="mobile" />
                 </div>
             </motion.div>
+            <div className="md:hidden flex flex-col sm:flex-row flex-wrap justify-center gap-[2vw]">
+                <ProjectCard name="JPG Attendance" vidSource={isoUlt} platform="mobile" />
+                <ProjectCard name="NDDU Attendance" vidSource={neonUlt} platform="web" />
+                <ProjectCard name="Blaan App" vidSource={sovaUlt} platform="mobile" />
+            </div>
         </section>
     );
 };
